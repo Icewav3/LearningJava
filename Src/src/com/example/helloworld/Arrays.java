567d9030c3b3a17e9ee27cd43535dead35022311
Icewav3 <coledorman12@gmail.com>
2022-07-09 09:48:50 -0700

    array maker

--- Src/src/com/example/helloworld/Arrays.java
+++ Src/src/com/example/helloworld/Arrays.java
@@ -9,20 +9,31 @@ public class Arrays {
         return In.nextLine();
     }
         public static void main(String[]args){
-            String[] array = new String[0];
-            String input = user_input("Array Commands:\nadd\nprint\nremove\ndone");
-            switch (input){
-                case "print":
-                    for (int i = 0; i < array.length; i++){
-                        System.out.println(array[i]);
-                    }
-                case "add":
-                    String index = user_input("Input index");
-                    int index_int = Integer.parseInt(index);
-                    String value = user_input("Input value");
-                    array[index_int] = value;
-                case "done":
-                    break;
+            String[] array = new String[5];
+            String input = "a", index = "";
+            int index_int = 0;
+            while (!input.equals("done")){
+                input = user_input("Array Commands:\nadd\nprint\nremove\ndone");
+                switch (input){
+                    case "print":
+                        for (int i = 0; i < array.length; i++){
+                            System.out.println(array[i]);
+                        }
+                        break;
+                    case "add":
+                        index = user_input("Input index");
+                        index_int = Integer.parseInt(index);
+                        String value = user_input("Input value");
+                        array[index_int] = value;
+                        break;
+                    case "remove":
+                        index = user_input("Input index to remove");
+                        index_int = Integer.parseInt(index);
+                        array[index_int] = null;
+                        break;
+                    case "done":
+                        break;
+                }
             }
         }
 }
